a546f822a3d6ca83c684da38e3a0df63051dabbe
waterlink000 (Alexey F.) <waterlink000@gmail.com>
2013-07-09 23:20:41 +0300

    #15: Done search.
    * Improved profile params permit: extracted editable fields to config.
    * Added basic list of users.
    * Created simple search form with all search criterias.
    * Simple-minded form that contains every searchable parameter. Allows doing backend for search.
    * Added ransack. This gem simplifies generating of queries to db for search.
    * Moved controller's javascript to controllers folder.
    * Better way to get rails controller and action on client side.
    * Added search mechanism.
    * Made lib with ransack-like predicates for hstore: only predicates that are required for search form.
    * Made lib providing form_for_hash helper: just like form_for, but for hash.
    * Added basic controller tests
    * #15: Added search and search results headers.
    * #15: Worked on search results view.
    * Done with search.
    * Improved search results view (description character limit, better align); fixed 'Show profile' button.
    * Extracted criteria view into partial template.
    * Made two sections of criterias: :primary and :hidden. Hidden are collapsed by default. Expanded by 'All criterias' button.
    * Sex, age and location goes first as :primary and everything else follows as :hidden.
    * Fixed HashFormObject to handle 'sym1[sym2[sym3[sym4]]]' symbols as 'hash[sym1][sym2][sym3][sym4]'.
    * Made hstore's search_cont predicate to be case insensitive.
    * Fixed active admin css problems.
    * Refactored user controller.
    * #hotfix: issue with int fields in hstore: solved with cast for now.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -14,4 +14,19 @@ class UsersControllerTest < ActionController::TestCase
     get :index
     assert_redirected_to edit_profile_path
   end
+
+  test 'should be able to see search form' do
+    sign_in users(:paul)
+
+    get :index
+    assert_response :success
+    assert_select 'input[value="Search"]', true
+  end
+
+  test 'should be able to search' do
+    sign_in users(:paul)
+
+    get :search, { search: {} }
+    assert_response :success
+  end
 end
