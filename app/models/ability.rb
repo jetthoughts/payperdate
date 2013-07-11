aea70e8dd801fc50013f02e8649b12cf209c5c32
Paul Nikitochkin <paul.nikitochkin@gmail.com>
2013-07-11 17:08:42 +0300

    #19: Refactroed Abilities to use more simple syntax. Add ability testing for managing profile to upload avatar.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,18 +7,14 @@ class Ability
     if user.present?
       can :read, Album
       can :read, Photo
+
       can :create, Album
       can :create, Photo
 
+      can :manage, Album, user_id: user.id, system: false
+      can :manage, Photo, user_id: user.id
+      can :manage, Avatar, user_id: user.id
       can :manage, Profile, user_id: user.id
-
-      can :manage, Album do | subject, params |
-        subject.user_id == user.id && !subject.system?
-      end
-
-      can :manage, Photo do | subject, params |
-        subject.user_id == user.id
-      end
     end
   end
 end
