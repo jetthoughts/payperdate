e35e82b6f8927599d18378036eab79de102e8d97
Alexey Fedorov <waterlink000@gmail.com>
2013-07-17 12:23:15 +0300

    #22: Admin photo panel: Approve/Decline Photos. * Added explicit action buttons for approve, decline, unapprove, undecline (last two return photo to penging scope). * Send non-delayed message to user about declined photos if it is not production environment. * Made notification about decline should be sent now instantly or in backgroung depenging on environment. * Fixed message sending. Added every scope with Approve/Decline status. * Made nudity job to be delayed only if production. * Fixed delayed jobs; Made non-approved photos appear only for owner. * Removed default from pending scope. Removed unapprove/undecline actions. Removed safe_for scope, replaced with public_photo relation. * Removed every scope. Using just all scope in admin. * Added debug output to setup_sample_data rake task.

--- lib/payperdate/nudity/impl/nude_impl.rb
+++ lib/payperdate/nudity/impl/nude_impl.rb
@@ -3,6 +3,9 @@ require 'nude'
 module NudityDetectorService
   class NudeImpl
     def nude?(image_path)
+      if Rails.env.development?
+        image_path = "public/#{image_path}"
+      end
       Nude.nude?(image_path)
     end
   end
