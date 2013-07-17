d718db4f299d39d0144d05e00d7cbb1740c28d48
Alexey Fedorov <waterlink000@gmail.com>
2013-07-17 12:23:15 +0300

    #26: Admin avatar panel: Approve/Decline Avatars. * Added explicit action buttons for approve, decline, unapprove, undecline (last two return photo to penging scope). * Send non-delayed message to user about declined photos if it is not production environment. * Made notification about decline should be sent now instantly or in backgroung depenging on environment. * Fixed message sending. Added every scope with Approve/Decline status. * Made nudity job to be delayed only if production. * Added debug output to setup_sample_data rake task. * Fixed delayed jobs; Made non-approved photos appear only for owner. * Fixed test_schedule_nudity_validation test to work. * Added basic permission tests for Admin-Avatars. Added Admin-Avatars controller. * Added declined reason: unknown, by face (only for avatar), by nudity. * Added column with face status.

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -3,6 +3,14 @@ class Avatar < Photo
 
   before_validation :find_album
 
+  scope :avatars, -> {
+    joins(:profile).where('profiles.avatar_id = photos.id')
+  }
+
+  scope :pending, -> { Photo.pending.avatars }
+  scope :approved, -> { Photo.approved.avatars }
+  scope :declined, -> { Photo.declined.avatars }
+
   private
 
   def find_album
