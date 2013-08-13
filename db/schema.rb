da05c302a7c2ed08f60232b575fb8a15497545dc
Miroslav Martynov <marty.mircha@gmail.com>
2013-08-13 15:15:28 +0300

    #76: fixed inner layouts

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20130808083702) do
 
-  #These are extensions that must be enabled in order to support this database
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
 
