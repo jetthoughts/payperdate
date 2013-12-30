d1df3e774ba9192bf0102a1e75127be6cbad4afd
Alexey Kisel <aki@jetthoughts.com>
2013-12-30 20:34:24 +0200

    returned timeou

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,2 @@
-unless %w(development test).include? Rails.env
-  Sidekiq.redis do |redis|
-    options = {cache: redis, cache_prefix: 'geocoder', lookup: :google}
-    Geocoder.configure options
-  end
-end
+options = {lookup: :google, timeout: 20}
+Geocoder.configure options
